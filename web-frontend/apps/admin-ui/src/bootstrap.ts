4cb64ddd3aebf2f52763de3099a7f5d6e6c2c41a
XIETONGJIAN <583110978@qq.com>
2026-07-15 18:45:08 +0800

    fix:统一vben弹窗属性配置

--- web-frontend/apps/admin-ui/src/bootstrap.ts
+++ web-frontend/apps/admin-ui/src/bootstrap.ts
@@ -1,7 +1,7 @@
 import { createApp, watchEffect } from 'vue';
 
 import { registerAccessDirective } from '@vben/access';
-import { registerLoadingDirective } from '@vben/common-ui';
+import { registerLoadingDirective, setDefaultModalProps } from '@vben/common-ui';
 import { preferences } from '@vben/preferences';
 import { initStores } from '@vben/stores';
 import '@vben/styles';
@@ -24,10 +24,11 @@ async function bootstrap(namespace: string) {
   // 初始化表单组件
   await initSetupVbenForm();
 
-  // // 设置弹窗的默认配置
-  // setDefaultModalProps({
-  //   fullscreenButton: false,
-  // });
+  // 设置弹窗的默认配置
+  setDefaultModalProps({
+    fullscreenButton: false,
+    draggable: true,
+  });
   // // 设置抽屉的默认配置
   // setDefaultDrawerProps({
   //   zIndex: 2000,
