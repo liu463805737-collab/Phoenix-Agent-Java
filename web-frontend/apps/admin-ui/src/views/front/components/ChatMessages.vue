38491837922c58962d737658d46b27d2f6f31d2a
XIETONGJIAN <583110978@qq.com>
2026-07-18 12:40:32 +0800

    fix:AI输出的内容只显示一次头像

--- web-frontend/apps/admin-ui/src/views/front/components/ChatMessages.vue
+++ web-frontend/apps/admin-ui/src/views/front/components/ChatMessages.vue
@@ -57,11 +57,18 @@ watch(activeSessionId, () => {
   <div ref="scrollRef" class="chat-messages">
     <div class="chat-messages__inner">
       <div
-        v-for="msg in activeMessages"
+··        v-for="(msg, index) in activeMessages"
         :key="msg.id"
         :class="['chat-message', msg.role]"
       >
-        <div v-if="msg.role === 'assistant'" class="chat-message__avatar">
+        <div
+          v-if="msg.role === 'assistant'"
+          class="chat-message__avatar"
+          :class="{
+            'chat-message__avatar--hidden':
+              index > 0 && activeMessages[index - 1].role === 'assistant',
+          }"
+        >
           {{ botName.charAt(0) }}
         </div>
 
@@ -183,6 +190,10 @@ watch(activeSessionId, () => {
 
   &.assistant &__content {
     width: 91.5%;
+  }
+
+  &__avatar--hidden {
+    visibility: hidden;
   }
 
   &__content {
