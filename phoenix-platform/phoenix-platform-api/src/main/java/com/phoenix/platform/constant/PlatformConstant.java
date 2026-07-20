77e35fd35ddd451639b94e660c028dbfc42e1f4a
freelander <857474261@qq.com>
2026-07-20 09:46:33 +0800

    feat: 移除不必要的建表语句

--- phoenix-platform/phoenix-platform-api/src/main/java/com/phoenix/platform/constant/PlatformConstant.java
+++ phoenix-platform/phoenix-platform-api/src/main/java/com/phoenix/platform/constant/PlatformConstant.java
@@ -4,7 +4,7 @@ public interface PlatformConstant {
     String PASSWORD_SALT = "phoenix";
 
     /** 默认组织编码 */
-    String GROUP_SN_COMMON = "common";
+    String GROUP_SN_COMMON = "COMMON";
 
     String ACCOUNT_LOGIN = "account_login";
 }
